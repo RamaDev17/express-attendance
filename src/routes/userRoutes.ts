5ce13678f7e0aba617f24ec94993af57d7cd5398
RamaDev17 <ramdhaney.dev17@gmail.com>
2024-10-21 08:12:33 +0700

    feat: add midleware checkrole in getDetailProfile

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getDetailUser } from "../controllers/userController";
 import { verifyToken } from "../middlewares/authMiddleware";
+import { checkRole } from "../middlewares/checkRole";
+import { Roles } from "../utils/roles";
 
 const router = Router();
 
-router.get("/profile", verifyToken, getDetailUser)
+router.get("/profile", verifyToken, checkRole([Roles.all]), getDetailUser)
 
 export default router
