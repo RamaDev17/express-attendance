61fd19ca1d9499fe66da5600ea2827d9390f2913
RamaDev17 <ramdhaney.dev17@gmail.com>
2024-11-18 10:14:33 +0700

    feat: add route validate and logout

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { login } from "../controllers/authController";
+import { login, logout, validateToken } from "../controllers/authController";
 
 const router = Router();
 
 router.post("/login", login)
+router.get("/validate", validateToken)
+router.post("/logout", logout)
 
 export default router
